ae54f8d0bea26ce2805df854b0a532fd9e4aecd6
Lich2013 <longzy2013@gmail.com>
2014-11-24 03:51:43 +0800

    finished

--- app/routes.php
+++ app/routes.php
@@ -16,10 +16,12 @@ Route::get('/{game}', 'HomeController@start');
 Route::post('/post', 'HomeController@verify');
 
 // 2048 Index
-Route::get("/main", function(){
+Route::get("/{game}/2048_main", function(){
 	return View::make("2048.main");
 });
 // 2048 guide
-Route::get("/guide", function(){
+Route::get("/{game}/2048_guide", function(){
 	return View::make("2048.guide");
 });
+
+Route::get("/{game}/2048_index", 'HomeController@start');
