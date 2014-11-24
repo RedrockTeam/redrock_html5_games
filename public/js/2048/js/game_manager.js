45228803da466550c83cd6d17ee7530120f00c16
董小姐 <dongtiangche@gmail.com>
2014-11-24 12:47:09 +0800

    bug

--- public/js/2048/js/game_manager.js
+++ public/js/2048/js/game_manager.js
@@ -401,26 +401,88 @@ GameManager.prototype.share = function(){
 
 	$("#reply").on("click", function(){
 		var phone = $("#phone_input").val();
-		$.ajax({
-			url : "/game/public/post",
-			type : "post",
-			dataType : 'json',
-			contentType : "application/json",
-			data :JSON.stringify({
-				_token: token,
-				name: name,
-				appid: appid,
-				score: score,
-				phone: phone,
-                time : time,
-				type: 2048
-			})
-		}).fail(function () {
-			alert("与服务器连接错误!");
-		}).complete(function (data) {
-            console.log(data);
-			data = data.responseJSON;
-			var myPlace;
+
+		if(! phone){
+			WeixinApi.ready(function(Api) {
+				var container = document.querySelector(".container"),
+					appid = container.dataset['appid'],
+					imgUrl  = container.dataset['imgurl'],
+					link  = container.dataset['link'],
+					name = container.dataset['name'];
+
+//                    myPlace = myPlace < 0 ? "N" : myPlace;
+				// 微信分享的数据
+				var wxData = {
+					"appId": "2048", // 服务号可以填写appId
+					"imgUrl" : imgUrl, // 二维码的地址
+					"link" : link,
+					"desc" : "我在“拼拼价值观”游戏中以" + time +  "秒时间，" + score + '积分取得了胜利',
+					"title" : "拼拼价值观"
+				};
+
+				// alert(wxData);
+				// 分享的回调
+				var wxCallbacks = {
+					// 分享操作开始之前
+					ready : function() {
+						// 你可以在这里对分享的数据进行重组
+					},
+					// 分享被用户自动取消
+					cancel : function(resp) {
+						// 你可以在你的页面上给用户一个小Tip，为什么要取消呢？
+						alert("o(>﹏<)o 为什么要取消呢？");
+					},
+					// 分享失败了
+					fail : function(resp) {
+						// 分享失败了，是不是可以告诉用户：不要紧，可能是网络问题，一会儿再试试？
+						alert("哎呀，o(>﹏<)o失败了");
+					},
+					// 分享成功
+					confirm : function(resp) {
+						// 分享成功了，我们是不是可以做一些分享统计呢？
+						alert("分享成功，<(￣▽￣)>");
+					},
+					// 整个分享过程结束
+					all : function(resp,shareTo) {
+						// 如果你做的是一个鼓励用户进行分享的产品，在这里是不是可以给用户一些反馈了？
+						// alert("分享" + (shareTo ? "到" + shareTo : "") + "结束，msg=" + resp.err_msg);
+					}
+				};
+				// 用户点开右上角popup菜单后，点击分享给好友，会执行下面这个代码
+				Api.shareToFriend(wxData, wxCallbacks);
+
+				// 点击分享到朋友圈，会执行下面这个代码
+				Api.shareToTimeline(wxData, wxCallbacks);
+
+				// 点击分享到腾讯微博，会执行下面这个代码
+				Api.shareToWeibo(wxData, wxCallbacks);
+
+				// iOS上，可以直接调用这个API进行分享，一句话搞定
+				Api.generalShare(wxData,wxCallbacks);
+				alert("内容已经粘贴到粘贴板， 快点击右上角的按钮分享吧！(≧▽≦)/");
+			});
+		}
+		else{
+			$.ajax({
+				url : "/game/public/post",
+				type : "post",
+				dataType : 'json',
+				contentType : "application/json",
+				data :JSON.stringify({
+					_token: token,
+					name: name,
+					appid: appid,
+					score: score,
+					phone: phone,
+					time : time,
+					type: 2048
+				})
+			}).fail(function () {
+				alert("与服务器连接错误!");
+			}).complete(function (data) {
+				console.log(data);
+				data = data.responseJSON;
+				var myPlace;
 //
 //			data.forEach(function(value, index){
 //				if(value.telphone == phone){
@@ -428,67 +490,67 @@ GameManager.prototype.share = function(){
 //				}
 //			});
 
-			myPlace = data[0];
-			WeixinApi.ready(function(Api) {
-			      var container = document.querySelector(".container"),
-			          appid = container.dataset['appid'],
-			          imgUrl  = container.dataset['imgurl'],
-			          link  = container.dataset['link'],
-			          name = container.dataset['name'];
+				myPlace = data[0];
+				WeixinApi.ready(function(Api) {
+					var container = document.querySelector(".container"),
+						appid = container.dataset['appid'],
+						imgUrl  = container.dataset['imgurl'],
+						link  = container.dataset['link'],
+						name = container.dataset['name'];
 
 //                    myPlace = myPlace < 0 ? "N" : myPlace;
-			      // 微信分享的数据
-			      var wxData = {
-			          "appId": "2048", // 服务号可以填写appId
-			          "imgUrl" : imgUrl, // 二维码的地址
-			          "link" : link,
-			          "desc" : "我在“拼拼价值观”游戏中以" + time +  "秒时间，" + score + '积分取得了胜利，排名第' + myPlace + "名",
-			          "title" : "拼拼价值观"
-			      };
+					// 微信分享的数据
+					var wxData = {
+						"appId": "2048", // 服务号可以填写appId
+						"imgUrl" : imgUrl, // 二维码的地址
+						"link" : link,
+						"desc" : "我在“拼拼价值观”游戏中以" + time +  "秒时间，" + score + '积分取得了胜利，排名第' + myPlace + "名",
+						"title" : "拼拼价值观"
+					};
 
-			      // alert(wxData);
-			      // 分享的回调
-			      var wxCallbacks = {
-			          // 分享操作开始之前
-			          ready : function() {
-			              // 你可以在这里对分享的数据进行重组
-			          },
-			          // 分享被用户自动取消
-			          cancel : function(resp) {
-			              // 你可以在你的页面上给用户一个小Tip，为什么要取消呢？
-			              alert("o(>﹏<)o 为什么要取消呢？");
-			          },
-			          // 分享失败了
-			          fail : function(resp) {
-			              // 分享失败了，是不是可以告诉用户：不要紧，可能是网络问题，一会儿再试试？
-			              alert("哎呀，o(>﹏<)o失败了");
-			          },
-			          // 分享成功
-			          confirm : function(resp) {
-			              // 分享成功了，我们是不是可以做一些分享统计呢？
-			              alert("分享成功，<(￣▽￣)>");
-			          },
-			          // 整个分享过程结束
-			          all : function(resp,shareTo) {
-			              // 如果你做的是一个鼓励用户进行分享的产品，在这里是不是可以给用户一些反馈了？
-			              // alert("分享" + (shareTo ? "到" + shareTo : "") + "结束，msg=" + resp.err_msg);
-			          }
-			      };
-			        // 用户点开右上角popup菜单后，点击分享给好友，会执行下面这个代码
-			        Api.shareToFriend(wxData, wxCallbacks);
+					// alert(wxData);
+					// 分享的回调
+					var wxCallbacks = {
+						// 分享操作开始之前
+						ready : function() {
+							// 你可以在这里对分享的数据进行重组
+						},
+						// 分享被用户自动取消
+						cancel : function(resp) {
+							// 你可以在你的页面上给用户一个小Tip，为什么要取消呢？
+							alert("o(>﹏<)o 为什么要取消呢？");
+						},
+						// 分享失败了
+						fail : function(resp) {
+							// 分享失败了，是不是可以告诉用户：不要紧，可能是网络问题，一会儿再试试？
+							alert("哎呀，o(>﹏<)o失败了");
+						},
+						// 分享成功
+						confirm : function(resp) {
+							// 分享成功了，我们是不是可以做一些分享统计呢？
+							alert("分享成功，<(￣▽￣)>");
+						},
+						// 整个分享过程结束
+						all : function(resp,shareTo) {
+							// 如果你做的是一个鼓励用户进行分享的产品，在这里是不是可以给用户一些反馈了？
+							// alert("分享" + (shareTo ? "到" + shareTo : "") + "结束，msg=" + resp.err_msg);
+						}
+					};
+					// 用户点开右上角popup菜单后，点击分享给好友，会执行下面这个代码
+					Api.shareToFriend(wxData, wxCallbacks);
 
-			        // 点击分享到朋友圈，会执行下面这个代码
-			        Api.shareToTimeline(wxData, wxCallbacks);
+					// 点击分享到朋友圈，会执行下面这个代码
+					Api.shareToTimeline(wxData, wxCallbacks);
 
-			        // 点击分享到腾讯微博，会执行下面这个代码
-			        Api.shareToWeibo(wxData, wxCallbacks);
+					// 点击分享到腾讯微博，会执行下面这个代码
+					Api.shareToWeibo(wxData, wxCallbacks);
 
-			        // iOS上，可以直接调用这个API进行分享，一句话搞定
-			        Api.generalShare(wxData,wxCallbacks);
-			      alert("内容已经粘贴到粘贴板， 快点击右上角的按钮分享吧！(≧▽≦)/");
+					// iOS上，可以直接调用这个API进行分享，一句话搞定
+					Api.generalShare(wxData,wxCallbacks);
+					alert("内容已经粘贴到粘贴板， 快点击右上角的按钮分享吧！(≧▽≦)/");
+				});
 			});
-
-		});
+		}
 	});
 
 
