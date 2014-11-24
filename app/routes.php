a5e96b18f033428826c642e1292d4159fb63a81e
董小姐 <dongtiangche@gmail.com>
2014-11-24 19:51:55 +0800

    asdsadasd

--- app/routes.php
+++ app/routes.php
@@ -17,7 +17,7 @@ Route::post('/post', 'HomeController@verify');
 
 // 2048 Index
 Route::get("/{game}/2048_main", function(){
-	return View::make("2048.main");
+	return View::make("2048.main")->with(["arr" => ["url" => "http://202.202.43.41/game/public/2048/2048_main", "path" => "http://202.202.43.41/game/public/pic/2048.png"]]);
 });
 // 2048 guide
 Route::get("/{game}/2048_guide", function(){
