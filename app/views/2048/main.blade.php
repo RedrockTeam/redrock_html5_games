6240fa1684f983914514f53851328d2288ded456
andcall <dongtiangche@gmail.com>
2014-11-27 10:17:24 +0800

    add data

--- app/views/2048/main.blade.php
+++ app/views/2048/main.blade.php
@@ -94,5 +94,20 @@
    	});
 
    </script>
+   <!-- Piwik -->
+   <script type="text/javascript">
+     var _paq = _paq || [];
+	   _paq.push(["trackPageView"]);
+	     _paq.push(["enableLinkTracking"]);
+
+		   (function() {
+		       var u=(("https:" == document.location.protocol) ? "https" : "http") + "://202.202.43.41/piwik/";
+			       _paq.push(["setTrackerUrl", u+"piwik.php"]);
+				       _paq.push(["setSiteId", "11"]);
+					       var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+						       g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+							     })();
+								 </script>
+								 <!-- End Piwik Code -->
 </body>
 </html>
