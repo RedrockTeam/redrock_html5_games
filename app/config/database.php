bf92129d1cc230e1e99204f85a78d8145216acb8
Lich <longzy2013@gmail.com>
2015-04-08 21:09:59 +0800

    sd

--- app/config/database.php
+++ app/config/database.php
@@ -56,7 +56,7 @@ return array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
 			'database'  => 'game',
-			'username'  => 'root',
+			'username'  => 'redrock',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_general_ci',
