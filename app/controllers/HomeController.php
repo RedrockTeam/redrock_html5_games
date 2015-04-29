9d1c02ec1e852a9cde463762165c22081c8fb63f
Lich <longzy2013@gmail.com>
2015-04-29 11:38:54 +0800

    oauth

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -58,8 +58,8 @@ class HomeController extends BaseController {
                  return View::make('praise-xi.index');
 
               case 'takephotos':
-                  Redirect::to("www.baidu.com");
-                  return;
+                 header("location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=$this->appid&redirect_uri=http%3a%2f%2fhongyan.cqupt.edu.cn%2fgame%2fpublic%2frealtakephotos&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect");
+                return;
               case 'realtakephotos':
                   return $CODE;
                   return View::make('takephotos.index');
