d648dbdc91aa646f713c8d03bc219c52ef22f3f7
TruemenHale <truemenhale@Hales-MacBook-Pro.local>
2015-05-14 19:26:30 +0800

    写了点JS

--- public/js/goodcitizen/main.js
+++ public/js/goodcitizen/main.js
@@ -2,13 +2,64 @@
  * Created by truemenhale on 15/5/14.
  */
 //1.爱国;2.诚信;3.敬业;4.友善
-function gameInit(){
+function ballInit(){
 	this.type;
 	this.src;
 }
-gameInit.prototype.setType=function(a){
+ballInit.prototype.setType=function(a){
 		this.type=a;
 };
-gameInit.prototype.setSrc=function(src){
+ballInit.prototype.setSrc=function(src){
 	this.src=src;
 };
+function boxInit(){
+	ballInit.call(this);
+}
+for(x in ballInit.prototype){
+	boxInit.prototype[x]=ballInit.prototype[x];
+}
+function gameInit(balls,boxs){
+	for(var i=1 ; i<5 ; i++){
+		switch (i){
+			case 1:
+				for(var j=1 ; j<10 ; j++){
+					var a=new ballInit();
+					a.setType(i);
+					a.setSrc('../../images/goodcitizen/'+i+'/'+j+'.png');
+					balls.push(a);
+				}
+				break;
+			case 2:
+				for(var j=1 ; j<12 ; j++){
+					var a=new ballInit();
+					a.setType(i);
+					a.setSrc('../../images/goodcitizen/'+i+'/'+j+'.png');
+					balls.push(a);
+				}
+				break;
+			case 3:
+				for(var j=1 ; j<14 ; j++){
+					var a=new ballInit();
+					a.setType(i);
+					a.setSrc('../../images/goodcitizen/'+i+'/'+j+'.png');
+					balls.push(a);
+				}
+				break;
+			case 4:
+				for(var j=1 ; j<12 ; j++){
+					var a=new ballInit();
+					a.setType(i);
+					a.setSrc('../../images/goodcitizen/'+i+'/'+j+'.png');
+					balls.push(a);
+				}
+				break;
+			default:
+				alert('我要报警了！');
+		}
+	}
+}
+var balls=[];
+var boxs=[];
+$(function(){
+	gameInit(balls,boxs);
+});
