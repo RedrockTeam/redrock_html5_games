1de735ad2d853099a332d444b624e4f29d86818e
Lich <longzy2013@gmail.com>
2015-05-23 16:34:32 +0800

    ip

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -260,7 +260,7 @@ class HomeController extends BaseController {
             $data = array(
                 'time' => $input['time'],
                 'score'=> $input['score'],
-                'ip' => Request::getClientIp(),
+                'ip' => Request::ip(),
             );
             $id = Goodcitizen::create($data);
             $uid = $id['id'];
