bcfb29e36fcc8e7b6729a7edf27ef404f29f32cf
Lich <longzy2013@gmail.com>
2015-10-15 20:38:08 +0800

    ok

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '202.202.43.41',
+			'host'      => 'localhost',
 			'database'  => 'game',
 			'username'  => 'redrock',
 			'password'  => 'hongyanredrock',
