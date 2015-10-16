d0753c9d48b9da1d67ccfdf82ebfb619598999f5
Lich2013 <longzy2013@gmail.com>
2015-10-16 10:30:49 +0800

    debug

--- app/views/cqupt-group-photo/index.blade.php
+++ app/views/cqupt-group-photo/index.blade.php
@@ -60,7 +60,7 @@
         wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: "{{$appid}}", // 必填，公众号的唯一标识
-            timestamp: "{{$tickt['timestamp']}}", // 必填，生成签名的时间戳
+            timestamp: "{{$ticket['timestamp']}}", // 必填，生成签名的时间戳
             nonceStr: "{{$ticket['noncestr']}}", // 必填，生成签名的随机串
             signature: "{{$ticket['signature']}}",// 必填，签名，见附录1
             jsApiList: [
