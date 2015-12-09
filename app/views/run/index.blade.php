1d0c952a66359a10fc3ad7f875d96cb54a18b1ce
Lich2013 <longzy2013@gmail.com>
2015-12-09 12:41:23 +0800

    path

--- app/views/run/index.blade.php
+++ app/views/run/index.blade.php
@@ -2,28 +2,28 @@
 <html>
 <head lang="zh-hans">
     <meta charset="UTF-8">
-    <script src="http://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
+    <script src="//cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-    {{HTML::style("resource/run/css/style.css")}}
+    {{HTML::style("game/resource/run/css/style.css")}}
 <!--    <link rel="stylesheet" type="text/css" href="style.css">-->
     <title>奔跑吧兄弟</title>
-    {{HTML::script("resource/run/js/API.js")}}
+    {{HTML::script("game/resource/run/js/API.js")}}
 <!--    <script src="js/API.js"></script>-->
 <!--    <script src="js/normal.js"></script>-->
-    {{HTML::script("resource/run/js/normal.js")}}
+    {{HTML::script("game/resource/run/js/normal.js")}}
 </head>
 <body>
 <div id="a" class="container" data="{{$arr['_token']}}" data-link="{{$arr['url']}}"></div>
 <div id="container">
     <div id="score_list"><span class="scroe">当前得分<font id="ScoreNum"> 0 </font>分</span><span>当前时间<font id="time"> 0 </font>秒</span></div>
     <div id="loading">
-        {{HTML::image("resource/run/images/loading.jpg")}}
+        {{HTML::image("game/resource/run/images/loading.jpg")}}
 <!--        <img src="images/loading.jpg">-->
     </div>
     <div id="begin_web">
-        {{HTML::image("resource/run/images/begin_bg.jpg","tu",array('id'=>'begin_bg'))}}
+        {{HTML::image("game/resource/run/images/begin_bg.jpg","tu",array('id'=>'begin_bg'))}}
 <!--        <img src="images/begin_bg.jpg" id="begin_bg">-->
         <p id="btn_list">
             <input class="btn_normal" type="button" value="开始游戏" id="start"><br><br>
@@ -32,7 +32,7 @@
         <span class="copyright">©2014红岩网校</span>
     </div>
     <div id="tips_web">
-        {{HTML::image("resource/run/images/tips.jpg","tu",array('id'=>'tips_img'))}}
+        {{HTML::image("game/resource/run/images/tips.jpg","tu",array('id'=>'tips_img'))}}
 <!--        <img src="images/tips.jpg" id="tips_img">-->
         <p class="tips_btn">
             <input class="btn_normal" type="button" value="开始游戏" id="tip_start">
@@ -42,7 +42,7 @@
     <canvas id="canvas" style="visibility: hidden;" width="320" height="480">
     </canvas>
     <div id="share">
-        {{HTML::image("resource/run/images/share.jpg","tu",array('id'=>'share_img'))}}
+        {{HTML::image("game/resource/run/images/share.jpg","tu",array('id'=>'share_img'))}}
 <!--        <img src="images/share.jpg" id="share_img">-->
         <p class="re_line">
             <input type="button" value="再来一次" id="reload">
