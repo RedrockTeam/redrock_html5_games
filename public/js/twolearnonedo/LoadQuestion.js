609d447657d786f233659c2f481ed2ace74cf7e6
Lich2013 <longzy2013@gmail.com>
2016-05-24 17:17:29 +0800

    test

--- public/js/twolearnonedo/LoadQuestion.js
+++ public/js/twolearnonedo/LoadQuestion.js
@@ -39,7 +39,7 @@ function isSame(x,s){
     return 0;
 }
 $(function(){
-    var imgs = ["images/twolearnonedo/lead_back.png","images/twolearnonedo/begin-btn.png","images/nextBtn.png"];
+    var imgs = ["images/twolearnonedo/lead_back.png","images/twolearnonedo/begin-btn.png","images/twolearnonedo/nextBtn.png"];
     //虚拟测试
     for(var i = 0,len = _data.length; i<len; i++){
         imgs.push(_data[i].pic);
