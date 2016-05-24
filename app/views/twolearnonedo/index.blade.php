0245fccbb1496dd3735394530631e08c871196d6
Lich2013 <longzy2013@gmail.com>
2016-05-24 21:08:48 +0800

    ok

--- app/views/twolearnonedo/index.blade.php
+++ app/views/twolearnonedo/index.blade.php
@@ -89,12 +89,12 @@
 <script src="https://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
     <script>
         var title = "我正在参与时代先锋问问答, 你也加入吧, 有大奖哟！";
-        var link = "{{$ticket['url']}}";
+        var link = "https://redrock.cqupt.edu.cn/game/twolearnonedo";
         var imgUrl = "https://redrock.cqupt.edu.cn/game/images/twolearnonedo/lead_back.png";
         var desc = "";
         //        jssdk
         wx.config({
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: "{{$appid}}", // 必填，公众号的唯一标识
             timestamp: "{{$ticket['timestamp']}}", // 必填，生成签名的时间戳
             nonceStr: "{{$ticket['noncestr']}}", // 必填，生成签名的随机串
