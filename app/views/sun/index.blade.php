9b6b69e57280ea45bd5110ee68653c312fcdede4
Lich2013 <longzy2013@gmail.com>
2016-05-31 18:20:02 +0800

    disable piwik

--- app/views/sun/index.blade.php
+++ app/views/sun/index.blade.php
@@ -44,18 +44,18 @@
     </div>
 </body>
 <!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-    _paq.push(["trackPageView"]);
-	  _paq.push(["enableLinkTracking"]);
+{{--<script type="text/javascript">--}}
+  {{--var _paq = _paq || [];--}}
+    {{--_paq.push(["trackPageView"]);--}}
+	  {{--_paq.push(["enableLinkTracking"]);--}}
 
-	    (function() {
-		    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://202.202.43.41/piwik/";
-			    _paq.push(["setTrackerUrl", u+"piwik.php"]);
-				    _paq.push(["setSiteId", "12"]);
-					    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-						    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-							  })();
-							  </script>
+	    {{--(function() {--}}
+		    {{--var u=(("https:" == document.location.protocol) ? "https" : "http") + "://202.202.43.41/piwik/";--}}
+			    {{--_paq.push(["setTrackerUrl", u+"piwik.php"]);--}}
+				    {{--_paq.push(["setSiteId", "12"]);--}}
+					    {{--var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";--}}
+						    {{--g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);--}}
+							  {{--})();--}}
+							  {{--</script>--}}
 							  <!-- End Piwik Code -->
 </html>
