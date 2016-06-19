440d47630d61115a6efab8b77e1268ad6787aec6
Lich2013 <longzy2013@gmail.com>
2016-06-19 23:57:10 +0800

    jssdk ok

--- app/views/party/index.blade.php
+++ app/views/party/index.blade.php
@@ -79,7 +79,7 @@
         var desc = "我爱学党章的描述, 还没想好填什么";
         //        jssdk
         wx.config({
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: "{{$appid}}", // 必填，公众号的唯一标识
             timestamp: "{{$ticket['timestamp']}}", // 必填，生成签名的时间戳
             nonceStr: "{{$ticket['noncestr']}}", // 必填，生成签名的随机串
