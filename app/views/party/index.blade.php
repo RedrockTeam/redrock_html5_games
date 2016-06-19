117f6c2ea12cceafb236a40487b89b8b4a52fbaf
Lich2013 <longzy2013@gmail.com>
2016-06-19 23:56:31 +0800

    jssdk test

--- app/views/party/index.blade.php
+++ app/views/party/index.blade.php
@@ -71,5 +71,64 @@
         </div>
         <p class="copyright">©红岩网校工作站</p>
     </div>
+    <script src="https://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+    <script>
+        var title = "我正在参加 “我爱学党章”微信游戏争霸赛，你也加入吧！";
+        var link = "https://redrock.cqupt.edu.cn/game/learnpartyconstitution";
+        var imgUrl = "https://redrock.cqupt.edu.cn/game/images/party/sSlogan.png";
+        var desc = "我爱学党章的描述, 还没想好填什么";
+        //        jssdk
+        wx.config({
+            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: "{{$appid}}", // 必填，公众号的唯一标识
+            timestamp: "{{$ticket['timestamp']}}", // 必填，生成签名的时间戳
+            nonceStr: "{{$ticket['noncestr']}}", // 必填，生成签名的随机串
+            signature: "{{$ticket['signature']}}",// 必填，签名，见附录1
+            jsApiList: [
+                'onMenuShareTimeline',
+                'onMenuShareAppMessage',
+                'onMenuShareQQ',
+            ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        });
+        wx.ready(function(){
+            wx.onMenuShareTimeline({
+                title: title, // 分享标题
+                link: link,
+                imgUrl: imgUrl,
+                success: function () {
+                    // 用户确认分享后执行的回调函数
+                },
+                cancel: function () {
+                    // 用户取消分享后执行的回调函数
+                }
+            });
+            wx.onMenuShareAppMessage({
+                title: title, // 分享标题
+                desc: desc, // 分享描述
+                link: link,
+                imgUrl: imgUrl, // 分享图标
+                type: '', // 分享类型,music、video或link，不填默认为link
+                dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                success: function () {
+                    // 用户确认分享后执行的回调函数
+                },
+                cancel: function () {
+                    // 用户取消分享后执行的回调函数
+                }
+            });
+            wx.onMenuShareQQ({
+                title: title, // 分享标题
+                desc: desc, // 分享描述
+                link: link,
+                imgUrl: imgUrl, // 分享图标
+                success: function () {
+                    // 用户确认分享后执行的回调函数
+                },
+                cancel: function () {
+                    // 用户取消分享后执行的回调函数
+                }
+            });
+        });
+    </script>
 </body>
 </html>
